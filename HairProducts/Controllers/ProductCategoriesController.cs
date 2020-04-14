a32082468c4f3581f7a1a5db71f27942121b7224
AjayKumar <akgorsi8055@gmail.com>
2020-04-14 20:30:26 +0300

    ProductCategories controller and views created

---
+++ HairProducts/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairProducts.Data;
+using HairProducts.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HairProducts.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductCategories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductCategory.ToListAsync());
+        }
+
+        // GET: ProductCategories/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        [Authorize]
+        // GET: ProductCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,CategoryDescription")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+            return View(productCategory);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("CategoryId,CategoryName,CategoryDescription")] ProductCategory productCategory)
+        {
+            if (id != productCategory.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductCategoryExists(productCategory.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productCategory);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategory
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            _context.ProductCategory.Remove(productCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductCategoryExists(long id)
+        {
+            return _context.ProductCategory.Any(e => e.CategoryId == id);
+        }
+    }
+}
