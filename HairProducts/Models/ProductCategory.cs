35cb7405368e05d116a9f9d4de40f0079e301a72
AjayKumar <akgorsi8055@gmail.com>
2020-04-14 20:27:44 +0300

    Models Created

---
+++ HairProducts/Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairProducts.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public long CategoryId { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Category name cannot be more than 100 characters")]
+        [MinLength(1, ErrorMessage = "Category name cannot be empty")]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+        [Required]
+        [Display(Name = "Category Description")]
+        public string CategoryDescription { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ HairProducts/Models/Products.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairProducts.Models
+{
+    public class Product
+    {
+        [Key]
+        public long ProductId { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Product name cannot be more than 100 characters")]
+        [MinLength(1, ErrorMessage = "Product name cannot be empty")]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Required]
+        [Display(Name = "Product Description")]
+        public string ProductDescription { get; set; }
+        [Required]
+        [Display(Name = "Product Price")]
+        [NotMapped]
+        public IFormFile Upload { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+        [Required]
+        public decimal ProductPrice { get; set; }
+        public long ProductCategoryId { get; set; }
+        public virtual ProductCategory ProductCategory { get; set; }
+    }
+}
