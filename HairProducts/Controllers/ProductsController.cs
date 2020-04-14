6d1aa0ae634c8da8698ab42765b9a443cde231db
AjayKumar <akgorsi8055@gmail.com>
2020-04-14 20:29:30 +0300

    Products Controller and views created

---
+++ HairProducts/Controllers/ProductsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairProducts.Data;
+using HairProducts.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HairProducts.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Product.Include(p => p.ProductCategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductCategory)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            ViewData["ProductCategoryId"] = new SelectList(_context.ProductCategory, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductDescription,Image,ProductPrice,ProductCategoryId,Upload")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Stream stream = product.Upload.OpenReadStream();
+                stream.Position = 0;
+                byte[] image = new byte[stream.Length];
+                stream.Read(image, 0, (int)stream.Length);
+                product.Image = image;
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductCategoryId"] = new SelectList(_context.ProductCategory, "CategoryId", "CategoryName", product.ProductCategoryId);
+            return View(product);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductCategoryId"] = new SelectList(_context.ProductCategory, "CategoryId", "CategoryName", product.ProductCategoryId);
+            return View(product);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ProductId,ProductName,ProductDescription,Image,ProductPrice,ProductCategoryId,Upload")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Stream stream = product.Upload.OpenReadStream();
+                    stream.Position = 0;
+                    byte[] image = new byte[stream.Length];
+                    stream.Read(image, 0, (int)stream.Length);
+                    product.Image = image;
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductCategoryId"] = new SelectList(_context.ProductCategory, "CategoryId", "CategoryName", product.ProductCategoryId);
+            return View(product);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductCategory)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(long id)
+        {
+            return _context.Product.Any(e => e.ProductId == id);
+        }
+    }
+}
