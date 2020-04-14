9eed3c8cd4022d3db479dd169534bc840fe1ec03
AjayKumar <akgorsi8055@gmail.com>
2020-04-14 21:22:35 +0300

    API Created

---
+++ HairProducts/Controllers/APIController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HairProducts.Data;
+using HairProducts.Models;
+
+namespace HairProducts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public APIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/API
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategory()
+        {
+            return await _context.ProductCategory.ToListAsync();
+        }
+
+        // GET: api/API/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductCategory>> GetProductCategory(long id)
+        {
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return productCategory;
+        }
+
+        // PUT: api/API/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductCategory(long id, ProductCategory productCategory)
+        {
+            if (id != productCategory.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productCategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/API
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<ProductCategory>> PostProductCategory(ProductCategory productCategory)
+        {
+            _context.ProductCategory.Add(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductCategory", new { id = productCategory.CategoryId }, productCategory);
+        }
+
+        // DELETE: api/API/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductCategory>> DeleteProductCategory(long id)
+        {
+            var productCategory = await _context.ProductCategory.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductCategory.Remove(productCategory);
+            await _context.SaveChangesAsync();
+
+            return productCategory;
+        }
+
+        private bool ProductCategoryExists(long id)
+        {
+            return _context.ProductCategory.Any(e => e.CategoryId == id);
+        }
+    }
+}
