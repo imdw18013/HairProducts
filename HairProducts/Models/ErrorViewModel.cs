ba350d50fab557d8aef4e3a436be11b2365c2afa
AjayKumar <akgorsi8055@gmail.com>
2020-04-14 20:21:59 +0300

    Add project files.

---
+++ HairProducts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HairProducts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
